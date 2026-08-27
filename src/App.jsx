94b2ef24f3e7b7c62c20a60ebfc8f4f2b525ffd5
João Paulo Oltramari <jpoltramari@gmail.com>
2026-08-27 11:43:34 -0300

    feat(books): add book details route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/auth/RegisterPage";
 
 import BooksPage from "./pages/books/BooksPage";
 import BookFormPage from "./pages/books/BookFormPage";
+import BookDetailPage from "./pages/books/BookDetailPage";
 
 import AuthorsPage from "./pages/authors/AuthorsPage";
 import AuthorFormPage from "./pages/authors/AuthorFormPage";
@@ -36,54 +37,25 @@ function AppRoutes() {
 
     return (
         <Routes>
-            <Route
-                path="/"
-                element={isAuthenticated ? <Navigate to="/home" replace /> : <LoginPage />}
-            />
-            <Route
-                path="/register"
-                element={isAuthenticated ? <Navigate to="/home" replace /> : <RegisterPage />}
-            />
+            <Route path="/" element={isAuthenticated ? <Navigate to="/home" replace /> : <LoginPage />} />
+            <Route path="/register" element={isAuthenticated ? <Navigate to="/home" replace /> : <RegisterPage />} />
 
             <Route path="/home" element={<WithLayout><HomePage /></WithLayout>} />
             <Route path="/books" element={<WithLayout><BooksPage /></WithLayout>} />
-            <Route
-                path="/books/new"
-                element={<WithPermission permission="BOOK_CREATE"><BookFormPage /></WithPermission>}
-            />
+            <Route path="/books/:id" element={<WithLayout><BookDetailPage /></WithLayout>} />
+            <Route path="/books/new" element={<WithPermission permission="BOOK_CREATE"><BookFormPage /></WithPermission>} />
             <Route path="/authors" element={<WithLayout><AuthorsPage /></WithLayout>} />
-            <Route
-                path="/authors/new"
-                element={<WithPermission permission="AUTHOR_CREATE"><AuthorFormPage /></WithPermission>}
-            />
+            <Route path="/authors/new" element={<WithPermission permission="AUTHOR_CREATE"><AuthorFormPage /></WithPermission>} />
 
             <Route path="/loans" element={<WithLayout><LoansPage showAll={false} /></WithLayout>} />
-            <Route
-                path="/loans/all"
-                element={<WithPermission permission="LOAN_READ_ALL"><LoansPage showAll /></WithPermission>}
-            />
+            <Route path="/loans/all" element={<WithPermission permission="LOAN_READ_ALL"><LoansPage showAll /></WithPermission>} />
             <Route path="/history" element={<WithLayout><HistoryPage showAll={false} /></WithLayout>} />
-            <Route
-                path="/history/all"
-                element={<WithPermission permission="LOAN_READ_ALL"><HistoryPage showAll /></WithPermission>}
-            />
+            <Route path="/history/all" element={<WithPermission permission="LOAN_READ_ALL"><HistoryPage showAll /></WithPermission>} />
 
-            <Route
-                path="/reports/most-borrowed"
-                element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="most-borrowed" /></WithPermission>}
-            />
-            <Route
-                path="/reports/recommended"
-                element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="recommended" /></WithPermission>}
-            />
-            <Route
-                path="/reports/categories"
-                element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="categories" /></WithPermission>}
-            />
-            <Route
-                path="/reports/active-users"
-                element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="active-users" /></WithPermission>}
-            />
+            <Route path="/reports/most-borrowed" element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="most-borrowed" /></WithPermission>} />
+            <Route path="/reports/recommended" element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="recommended" /></WithPermission>} />
+            <Route path="/reports/categories" element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="categories" /></WithPermission>} />
+            <Route path="/reports/active-users" element={<WithPermission permission="LOAN_READ_ALL"><ReportsPage type="active-users" /></WithPermission>} />
 
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
